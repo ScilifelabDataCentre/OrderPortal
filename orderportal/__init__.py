93a0642c50207d1f8fe6a3e6268d47df8b02ffe5
Per Kraulis <per.kraulis@scilifelab.se>
2015-07-01 10:34:48 +0200

    Bumped up version to 0.2.

--- orderportal/__init__.py
+++ orderportal/__init__.py
@@ -4,7 +4,7 @@ to a facility from its users.
 
 from __future__ import unicode_literals, print_function, absolute_import
 
-__version__ = '0.1'
+__version__ = '0.2'
 
 # Default settings, to be changed in a settings YAML file.
 settings = dict(BASE_URL='http://localhost:8885/',
