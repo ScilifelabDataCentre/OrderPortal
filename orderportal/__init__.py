9d8abd3a3f8ee2c95a98ca3d190c3d5fedc54327
Per Kraulis <per.kraulis@scilifelab.se>
2015-09-15 14:42:53 +0200

    Version 0.4. Closes #147.

--- orderportal/__init__.py
+++ orderportal/__init__.py
@@ -4,7 +4,7 @@ to a facility from its users.
 
 from __future__ import print_function, absolute_import
 
-__version__ = '0.3'
+__version__ = '0.4'
 
 # Default settings, to be changed in a settings YAML file.
 settings = dict(BASE_URL='http://localhost:8885/',
