ed53f0d1e2a62bf6b7bc76ead4f712ab271f0134
Per Kraulis <per.kraulis@scilifelab.se>
2015-10-09 17:14:01 +0200

    Global order messages implemented. Closes #200. Closes #201.

--- orderportal/scripts/messager.py
+++ orderportal/scripts/messager.py
@@ -8,6 +8,7 @@ import email.mime.text
 import smtplib
 import urllib
 
+import couchdb
 import yaml
 
 from orderportal import constants
@@ -36,6 +37,11 @@ class Messager(object):
                 self.account_messages = yaml.safe_load(infile)
         except (IOError, KeyError):
             self.account_messages = {}
+        try:
+            with open(settings['ORDER_MESSAGES_FILENAME']) as infile:
+                self.order_messages = yaml.safe_load(infile)
+        except (IOError, KeyError):
+            self.order_messages = {}
 
     @property
     def server(self):
@@ -83,6 +89,8 @@ class Messager(object):
                             startkey=constants.HIGH_CHAR,
                             endkey=endkey)
         for row in view:
+            if self.verbose:
+                print('log', row.id)
             if row.value == constants.ACCOUNT:
                 self.process_account_log(row.doc)
             elif row.value == constants.ORDER:
@@ -99,9 +107,12 @@ class Messager(object):
 
     def process_account_pending(self, logdoc):
         "Account was created, is pending. Tell the admins to enable it."
-        message = self.account_messages.get('account_pending')
+        message = self.account_messages.get('pending')
         if not message: return
-        account = self.db[logdoc['entity']]
+        try:
+            account = self.db[logdoc['entity']]
+        except couchdb.ResourceNotFound:
+            return
         params = dict(site=settings['SITE_NAME'],
                       account=account['email'],
                       url=self.absolute_url('account', account['email']))
@@ -109,9 +120,12 @@ class Messager(object):
 
     def process_account_enabled(self, logdoc):
         """Account was enabled. Send URL and code for setting password."""
-        message = self.account_messages.get('account_enabled')
+        message = self.account_messages.get('enabled')
         if not message: return
-        account = self.db[logdoc['entity']]
+        try:
+            account = self.db[logdoc['entity']]
+        except couchdb.ResourceNotFound:
+            return
         params = dict(site=settings['SITE_NAME'],
                       account=account['email'],
                       url=self.absolute_url('password'),
@@ -123,10 +137,37 @@ class Messager(object):
 
     def process_order_log(self, logdoc):
         "Send message for an interesting order log record."
-        print('order', logdoc['changed'])
+        status = logdoc['changed'].get('status')
+        message = self.order_messages.get(status)
+        if not message: return
+        try:
+            order = self.db[logdoc['entity']]
+        except couchdb.ResourceNotFound:
+            return
+        owner = self.get_account(order['owner'])
+        # Owner may have disappeared (OK, OK, unlikely)
+        if not owner: return
+        params = dict(site=settings['SITE_NAME'],
+                      owner=owner['email'],
+                      order=order.get('title') or order['_id'],
+                      url=self.absolute_url('order', order['_id']))
+        # Send to administrators, if so configured
+        for role in message['recipients']:
+            if role == 'admin':
+                self.send_email(self.get_admins(), message, params)
+                break
+        # Send to owner and group, if so configured
+        recipients = set()
+        for role in message['recipients']:
+            if role == 'owner':
+                recipients.add(owner['email'])
+            elif role == 'group':
+                recipients.update(self.get_colleagues(owner['email']))
+        self.send_email(list(recipients), message, params)
 
     def send_email(self, recipients, message, params):
         "Actually send the message as email; not if the dry_run flag is set."
+        if not recipients: return
         subject = message['subject'].format(**params)
         text = message['text'].format(**params)
         sender = settings['MESSAGE_SENDER_EMAIL']
@@ -135,24 +176,43 @@ class Messager(object):
         mail['From'] = sender
         for recipient in recipients:
             mail['To'] = recipient
-            if self.dry_run:
-                print(mail.as_string())
-            else:
-                self.server.sendmail(sender, [recipient], mail.as_string())
-                if self.verbose:
-                    print("sent email '{}' to {}".format(subject, recipient))
-        if not self.dry_run:
+        if self.dry_run:
+            print(mail.as_string())
+        else:
+            self.server.sendmail(sender, recipients, mail.as_string())
             with MessageSaver(db=self.db) as saver:
                 saver['sender'] = sender
                 saver['recipients'] = recipients
                 saver['subject'] = subject
                 saver['text'] = text
                 saver['type'] = 'email'
-        
+            if self.verbose:
+                print("sent email '{}' to {}".format(subject,
+                                                     ', '.join(recipients)))
+
+    def get_account(self, email):
+        "Get the account document for the email."
+        view = self.db.view('account/email', include_docs=True)
+        try:
+            return [r.doc for r in view[email]][0]
+        except IndexError:
+            return None
+
     def get_admins(self):
         "Get the list of enabled admin emails."
-        
-        return ['per.kraulis@scilifelab.se']
+        view = self.db.view('account/role', include_docs=True)
+        admins = [r.doc for r in view[constants.ADMIN]]
+        return [a['email'] for a in admins if a['status'] == constants.ENABLED]
+
+    def get_colleagues(self, email):
+        "Get list of emails for accounts in same groups as the given email."
+        colleagues = set()
+        for row in self.db.view('group/member', include_docs=True, key=email):
+            for member in row.doc['members']:
+                account = self.get_account(member)
+                if account['status'] == constants.ENABLED:
+                    colleagues.add(account['email'])
+        return list(colleagues)
 
 
 def get_args():
