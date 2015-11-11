80a9ddb06c18cb2be0707b2dacccee82649e7ee3
Per Kraulis <per.kraulis@scilifelab.se>
2015-11-11 16:29:05 +0100

    Included refresh JS. Closes #253.

--- orderportal/static/refresh_on_change.js
+++
@@ -1,8 +0,0 @@
-/* refresh_on_change.js
-   Refresh the page when a change in the input element of class "refresh".
-*/
-$(document).ready(function() {
-    $('.refresh').change(function () {
-	$('#refresh').submit();
-    });
-});
