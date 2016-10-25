48e61e22c02a2df0ee4fb839cb5e8f4f40a9d080
Per Kraulis <per.kraulis@scilifelab.se>
2016-10-25 14:49:12 +0200

    Bumped version.

--- orderportal/__init__.py
+++ orderportal/__init__.py
@@ -4,7 +4,7 @@ to a facility from its users.
 
 from __future__ import print_function, absolute_import
 
-__version__ = '2.8.1'
+__version__ = '2.8.2'
 
 # Default settings, may be changed in a settings YAML file.
 settings = dict(
