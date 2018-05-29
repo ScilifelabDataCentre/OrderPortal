7490be9cef5bf8952efdff9fcb066833eae5fdcc
Per Kraulis <per.kraulis@scilifelab.se>
2018-05-29 16:53:34 +0200

    Corrected API example script comment.

--- orderportal/scripts/api_edit_order.py
+++ orderportal/scripts/api_edit_order.py
@@ -1,7 +1,5 @@
 """API example script: Edit an order; fields, title, tags and history.
 
-NOTE: The upload operation URL requires the IUID for the order.
-
 NOTE: You need to change several variables to make this work. See below.
 
 NOTE: This uses the third-party 'requests' module, which is much nicer than
