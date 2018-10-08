ac4105f23ac67e1ae6cc15c46ef84993f2afda60
Per Kraulis <per.kraulis@scilifelab.se>
2018-10-08 13:25:30 +0200

    Tweaked table input. Closes #785.

--- orderportal/__init__.py
+++ orderportal/__init__.py
@@ -7,7 +7,7 @@ from __future__ import print_function, absolute_import
 import os
 
 
-__version__ = '3.6.6'
+__version__ = '3.6.7'
 
 # Default settings, may be changed in a settings YAML file.
 settings = dict(
