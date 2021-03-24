e9b56192b724d8a8d72553c02d80ce7ff506dd96
Per Kraulis <per.kraulis@gmail.com>
2021-03-24 20:02:29 +0100

    Added an API script.

--- orderportal/scripts/api_get_account.py
+++ orderportal/scripts/api_get_account.py
@@ -1,4 +1,4 @@
-"""API example script: Get order data.
+"""API example script: Get account data.
 
 NOTE: You need to change several variables to make this work. See below.
 
@@ -6,7 +6,7 @@ NOTE: This uses the third-party 'requests' module, which is better than
 the standard 'urllib' module.
 """
 
-import simplejson as json       # XXX Python 3 kludge
+import json
 
 # Third-party package: http://docs.python-requests.org/en/master/
 import requests
@@ -19,12 +19,12 @@ BASE_URL = 'https://ngisweden.scilifelab.se/orders'
 # API key for the user account. Must be changed (this is a dummy).
 API_KEY = '7f075a4c5b324e3ca63f22d8dc0929c4'
 
-# The ID for the order. The IUID can also be used.
-ORDER_ID = 'NGI10904'
+# The identifier for the account; an email address.
+ACCOUNT_ID = 'ngi_portal_api@scilifelab.se'
 
 
-url = "{base}/api/v1/order/{id}".format(base=BASE_URL,
-                                        id=ORDER_ID)
+url = "{base}/api/v1/account/{id}".format(base=BASE_URL,
+                                          id=ACCOUNT_ID)
 headers = {'X-OrderPortal-API-key': API_KEY}
 
 response = requests.get(url, headers=headers)
